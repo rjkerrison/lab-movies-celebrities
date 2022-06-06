b51b548e7422ded66f31c37e41459484d18eb183
caetanolmsouza <caetanomartins@hotmail.com>
2022-06-06 12:53:47 +0200

    first iteration

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,20 @@
 //  Add your code here
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const celebritySchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String,
+});
+
+const celebrities = [
+  { name: "celeb1", occupation: "art", catchPhrase: "one phrase" },
+  { name: "celeb2", occupation: "musician", catchPhrase: "nothing to say" },
+];
+
+const createdCelebs = await Celebrity.create(celebrities);
+
+const Celebrity = model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
